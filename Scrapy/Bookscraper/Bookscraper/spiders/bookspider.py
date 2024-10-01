a131a26d4db350d5a45b605782361d4357a7333f
Rayal Khatri <kcrayalgw@gmail.com>
2024-10-01 16:43:56 +0545

    Scrapy Project for book scraping starting code

---
+++ Scrapy/Bookscraper/Bookscraper/spiders/bookspider.py
@@ -0,0 +1,32 @@
+import scrapy
+
+
+class BookspiderSpider(scrapy.Spider):
+    name = "bookspider"
+    allowed_domains = ["books.toscrape.com"]
+    start_urls = ["https://books.toscrape.com/"]
+
+    def parse(self, response):
+        books = response.css('article.product_pod')
+        for book in books:
+            relative_url =book.css('h3 a::attr(href)').get()
+
+            if 'catalogue/' in relative_url:
+                book_url = "https://books.toscrape.com/" + relative_url
+            else:
+                book_url = "https://books.toscrape.com/catalogue/" + relative_url
+            yield response.follow(book_url, callback = self.parse_book_page)
+
+        next_page="https//books.to.scrape.com"
+        if next_page is not None:
+            if 'catalogue/' in next_page:
+                next_page_url = "https://books.toscrape.com/" + next_page
+            else:
+                next_page_url = "https://books.toscrape.com/catalogue/" + next_page
+
+            yield response.follow(next_page_url, callback = self.parse)
+
+
+    def parse_book_page(self, response):
+        Book_title = response.css("div.product_main h1::text").get()
+        Book_price = response.css("div.product_main p.price_color::text").get()
