f03b707b8049f46d1d8667a0f09283a6bda3c3cc
Rayal Khatri <kcrayalgw@gmail.com>
2024-10-02 13:17:27 +0545

    Using Pipelines and Bookitems to Clean the data

--- Scrapy/Bookscraper/Bookscraper/items.py
+++ Scrapy/Bookscraper/Bookscraper/items.py
@@ -7,5 +7,20 @@ import scrapy
 
 
 class BookscraperItem(scrapy.Item):
-   
     pass
+
+def serialize_price(value):
+    return f'£ {str(value)}'
+
+
+class BookItem(scrapy.Item):
+    url = scrapy.Field()
+    title= scrapy.Field()
+    product_type = scrapy.Field()
+    total_price = scrapy.Field()
+    tax =scrapy.Field()
+    availability = scrapy.Field()
+    no_of_reviews = scrapy.Field()
+    stars =scrapy.Field()
+    category = scrapy.Field()
+    description = scrapy.Field()
--- Scrapy/Bookscraper/Bookscraper/spiders/bookspider.py
+++ Scrapy/Bookscraper/Bookscraper/spiders/bookspider.py
@@ -1,5 +1,5 @@
 import scrapy
-
+from Bookscraper.items import BookItem
 
 class BookspiderSpider(scrapy.Spider):
     name = "bookspider"
@@ -18,6 +18,7 @@ class BookspiderSpider(scrapy.Spider):
             yield response.follow(book_url, callback = self.parse_book_page)
 
         next_page= response.css('li.next a::attr(href)').get()
+        # next_page=None
         if next_page is not None:
             if 'catalogue/' in next_page:
                 next_page_url = "https://books.toscrape.com/" + next_page
@@ -28,16 +29,16 @@ class BookspiderSpider(scrapy.Spider):
 
     def parse_book_page(self, response):
         table_rows= response.css("table tr")
-        yield{
-            'url' : response.url,
-            'title': response.css('.product_main h1::text').get(),
-            'product type' :table_rows[1].css('td::text').get(),
-            'Total price' : table_rows[3].css('td::text').get(),
-            'tax': table_rows[4].css('td::text').get(),    
-            "avaliability" : table_rows[5].css('td::text').get(),
-            "No. of reviews" : table_rows[6].css('td::text').get(),
-            "Stars" : response.css('p.star-rating').attrib['class'],
-            "Category" : response.xpath("//ul[@class='breadcrumb']/li[@class='active']/preceding-sibling::li[1]/a/text()").get(),
-            "Description" : response.xpath("//div[@id='product_description']/following-sibling::p/text()").get(),
-            "Price": response.css('p.price_color::text').get(),
-        }
+        book_item = BookItem()
+        book_item['url'] = response.url
+        book_item['title'] = response.css('.product_main h1::text').get()
+        book_item['product_type'] =table_rows[1].css('td::text').get()
+        book_item['total_price'] = table_rows[3].css('td::text').get()
+        book_item['tax'] = table_rows[4].css('td::text').get() 
+        book_item["availability"] = table_rows[5].css('td::text').get()
+        book_item["no_of_reviews"] = table_rows[6].css('td::text').get()
+        book_item["stars"] = response.css('p.star-rating').attrib['class']
+        book_item["category"] = response.xpath("//ul[@class='breadcrumb']/li[@class='active']/preceding-sibling::li[1]/a/text()").get()
+        book_item["description"] = response.xpath("//div[@id='product_description']/following-sibling::p/text()").get()
+        
+        yield book_item
