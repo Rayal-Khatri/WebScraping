58dbc8f1ff7d517a0f2d889b2e0317400b2ac262
Rayal Khatri <kcrayalgw@gmail.com>
2024-10-02 12:12:49 +0545

    Scrapes uncleaned data to csv

--- Scrapy/Bookscraper/Bookscraper/spiders/bookspider.py
+++ Scrapy/Bookscraper/Bookscraper/spiders/bookspider.py
@@ -17,16 +17,27 @@ class BookspiderSpider(scrapy.Spider):
                 book_url = "https://books.toscrape.com/catalogue/" + relative_url
             yield response.follow(book_url, callback = self.parse_book_page)
 
-        next_page="https//books.to.scrape.com"
+        next_page= response.css('li.next a::attr(href)').get()
         if next_page is not None:
             if 'catalogue/' in next_page:
                 next_page_url = "https://books.toscrape.com/" + next_page
             else:
                 next_page_url = "https://books.toscrape.com/catalogue/" + next_page
-
             yield response.follow(next_page_url, callback = self.parse)
 
 
     def parse_book_page(self, response):
-        Book_title = response.css("div.product_main h1::text").get()
-        Book_price = response.css("div.product_main p.price_color::text").get()
+        table_rows= response.css("table tr")
+        yield{
+            'url' : response.url,
+            'title': response.css('.product_main h1::text').get(),
+            'product type' :table_rows[1].css('td::text').get(),
+            'Total price' : table_rows[3].css('td::text').get(),
+            'tax': table_rows[4].css('td::text').get(),    
+            "avaliability" : table_rows[5].css('td::text').get(),
+            "No. of reviews" : table_rows[6].css('td::text').get(),
+            "Stars" : response.css('p.star-rating').attrib['class'],
+            "Category" : response.xpath("//ul[@class='breadcrumb']/li[@class='active']/preceding-sibling::li[1]/a/text()").get(),
+            "Description" : response.xpath("//div[@id='product_description']/following-sibling::p/text()").get(),
+            "Price": response.css('p.price_color::text').get(),
+        }
