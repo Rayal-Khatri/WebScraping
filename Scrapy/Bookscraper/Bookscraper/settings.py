1ed54892b1e0bbbebdc7217091bbb91ab63bce31
Rayal Khatri <kcrayalgw@gmail.com>
2024-10-03 23:52:04 +0545

    Adding Fake user agents using API

--- Scrapy/Bookscraper/Bookscraper/settings.py
+++ Scrapy/Bookscraper/Bookscraper/settings.py
@@ -16,6 +16,10 @@ FEEDS = {
     'books data.json': {'format':'json'}
 }
 
+SCRAPEOPS_API_KEY= 'f3dcf86d-22df-459b-9dde-7f5d55b37d55'
+SCRAPEOPS_FAKE_USER_AGENT_ENDPOINT ='https://headers.scrapeops.io/v1/user-agents'
+SCRAPEOPS_FAKE_USER_AGENT_ENABLED = True
+SCRAPEOPS_NUM_RESULTS = 50
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
 # USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/129.0.0.0 Safari/537.36"
@@ -54,9 +58,10 @@ ROBOTSTXT_OBEY = True
 
 # Enable or disable downloader middlewares
 # See https://docs.scrapy.org/en/latest/topics/downloader-middleware.html
-#DOWNLOADER_MIDDLEWARES = {
+DOWNLOADER_MIDDLEWARES = {
 #    "Bookscraper.middlewares.BookscraperDownloaderMiddleware": 543,
-#}
+   "Bookscraper.middlewares.FakeUserAgent": 400,
+}
 
 # Enable or disable extensions
 # See https://docs.scrapy.org/en/latest/topics/extensions.html
--- Scrapy/Bookscraper/Bookscraper/spiders/bookspider.py
+++ Scrapy/Bookscraper/Bookscraper/spiders/bookspider.py
@@ -7,19 +7,6 @@ class BookspiderSpider(scrapy.Spider):
     allowed_domains = ["books.toscrape.com"]
     start_urls = ["https://books.toscrape.com/"]
 
-    user_agent_list = [
-        'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36',
-        'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:124.0) Gecko/20100101 Firefox/124.0'
-        'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36 Edg/123.0.2420.81'
-        'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36 OPR/109.0.0.0'
-        'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36'
-        'Mozilla/5.0 (Macintosh; Intel Mac OS X 14.4; rv:124.0) Gecko/20100101 Firefox/124.0'
-        'Mozilla/5.0 (Macintosh; Intel Mac OS X 14_4_1) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/17.4.1 Safari/605.1.15'
-        'Mozilla/5.0 (Macintosh; Intel Mac OS X 14_4_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36 OPR/109.0.0.0'
-        'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36'
-        'Mozilla/5.0 (X11; Linux i686; rv:124.0) Gecko/20100101 Firefox/124.0'
-    ]
-
     def parse(self, response):
         books = response.css('article.product_pod')
         for book in books:
@@ -29,7 +16,7 @@ class BookspiderSpider(scrapy.Spider):
                 book_url = "https://books.toscrape.com/" + relative_url
             else:
                 book_url = "https://books.toscrape.com/catalogue/" + relative_url
-            yield response.follow(book_url, callback = self.parse_book_page, headers={"User-Agent":self.user_agent_list[random.randint(0,len(self.user_agent_list)-1)]})
+            yield response.follow(book_url, callback = self.parse_book_page)
 
         next_page= response.css('li.next a::attr(href)').get()
         next_page=None
@@ -38,7 +25,7 @@ class BookspiderSpider(scrapy.Spider):
                 next_page_url = "https://books.toscrape.com/" + next_page
             else:
                 next_page_url = "https://books.toscrape.com/catalogue/" + next_page
-            yield response.follow(next_page_url, callback = self.parse,headers={"User-Agent":self.user_agent_list[random.randint(0,len(self.user_agent_list)-1)]})
+            yield response.follow(next_page_url, callback = self.parse)
 
 
     def parse_book_page(self, response):
