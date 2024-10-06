e20a211da8c6a54c0dd1ba86172ab5aeb26a7f4c
Rayal Khatri <kcrayalgw@gmail.com>
2024-10-06 23:53:43 +0545

    pipelined the rates to change

--- Scrapy/chocolatescraper/chocolatescraper/pipelines.py
+++ Scrapy/chocolatescraper/chocolatescraper/pipelines.py
@@ -5,9 +5,40 @@
 
 
 # useful for handling different item types with a single interface
+from sqlite3 import adapters
 from itemadapter import ItemAdapter
-
+from scrapy.exceptions import DropItem
 
 class ChocolatescraperPipeline:
     def process_item(self, item, spider):
         return item
+
+
+
+class PriceUSDtoNRP:
+    Rate= 176.89
+    def process_item(self, item, spider):
+        adapter = ItemAdapter(item)
+
+        if adapter.get('price'):
+            floatprice = float(adapter.get('price'))
+            adapter['price'] = floatprice * self.Rate
+            return item
+        else:
+            raise DropItem(f"Missing price in {item}")
+        
+
+
+
+class DuplicatePipeline:
+    def __init__(self):
+        self.name_seen = set()
+    
+    def process_item(self, item, spider):
+        adapter = ItemAdapter(item)
+        
+        if adapter['name'] in self.name_seen:
+            raise DropItem(f"Duplicate item found: {item!r}")
+        else:
+             self.name_seen.add(adapter['name'])
+             return item
