b0e7951f6a570dbca052ca735b80759c4b7f97ed
Rayal Khatri <kcrayalgw@gmail.com>
2024-10-14 17:40:05 +0545

    Practise on navigating all the Pages

--- Scrapy/quotestoscrape/quotestoscrape/spiders/quotesspider.py
+++ Scrapy/quotestoscrape/quotestoscrape/spiders/quotesspider.py
@@ -1,6 +1,6 @@
 from typing import Iterable
 import scrapy
-from quotestoscrape.items import QuoteItem
+from ..items import QuoteItem
 from scrapy_playwright.page import PageMethod
 
 class QuotesspiderSpider(scrapy.Spider):
@@ -19,12 +19,18 @@ class QuotesspiderSpider(scrapy.Spider):
 
     
     def parse(self, response):
+
         for quote in response.css('div.quote'):
             quote_item = QuoteItem()
             quote_item['text'] = quote.css("span.text::text").get()
             quote_item['author'] = quote.css("small.author::text").get()
             quote_item['tags']= quote.css("a.tag::text").get()
             yield quote_item
+
+        next_page= response.css('.next a').xpath('@href').get()
+        if next_page is not None:
+            next_page_url = "https://quotes.toscrape.com" + next_page
+            yield scrapy.Request(url=next_page_url, callback = self.parse)
 
     async def errback(self, failure):
         page = failure.request.meta["playwrite_page"]
