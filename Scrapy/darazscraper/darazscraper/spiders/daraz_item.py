3ee3ef10f132ad7c6ad49f318ec39ba32162858a
Rayal Khatri <kcrayalgw@gmail.com>
2024-10-25 16:49:47 +0545

    Scraping the entime search results

--- Scrapy/darazscraper/darazscraper/spiders/daraz_item.py
+++ Scrapy/darazscraper/darazscraper/spiders/daraz_item.py
@@ -4,7 +4,6 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
 from scrapy.selector import Selector 
-import logging
 import time
 
 class DarazItemSpider(scrapy.Spider):
@@ -27,7 +26,11 @@ class DarazItemSpider(scrapy.Spider):
         driver.execute_script("window.scrollTo(0, document.body.scrollHeight / 1.2);")  # Wait for new content to load
 
         # Initial pagination button setup
+        a=0 #setting up a ocunter
         while True:
+            a+=1
+            if a <3:
+                break #stoping the loop at page 3... remove if you want to scrape all the items
             # Get and parse page source
             current_html = driver.page_source
             current_page_selector = Selector(text=current_html)
@@ -52,8 +55,42 @@ class DarazItemSpider(scrapy.Spider):
             # Scroll after navigation to load new items
             driver.execute_script("window.scrollTo(0, document.body.scrollHeight / 1.2);")
             time.sleep(1)  # Adjust if necessary to let content load fully
-        print(f"***************************************{len(items)}")
-        pass
+        print(f"***************************************{len(items)}")   
+        for item in items:
+            next_url = "https:"+ str(item.css('._95X4G a').xpath('@href').get())
+            yield SeleniumRequest(
+            url=next_url,
+            callback=self.parse_item_page,
+        )
+    
+    async def errback(self, failure):
+        page = failure.request.meta['playwright_page']
+        await page.close()
+
+    def parse_item_page(self, response):
+        driver = response.meta['driver']
+        print("*******************************************")
+
+        # Scroll to the middle of the page
+        driver.execute_script("window.scrollTo(0, document.body.scrollHeight / 2);")  # Scroll to the middle
+        time.sleep(3)
+        try:
+            rating = driver.find_element(By.CSS_SELECTOR, 'span.score-average').text.strip()
+        except Exception as e:
+            rating = 'No rating' 
+
+        yield {
+            'name': response.css('.pdp-mod-product-badge-title::text').get(),
+            'price': response.css('.pdp-price_size_xl::text').get(),
+            'rating':rating,
+            'no_of_rating': response.css('.pdp-review-summary__link::text').get(),
+            'delivery_price': response.css('.delivery-option-item_type_standard .no-subtitle::text').get(),
+            'seller': response.css('.seller-name__detail-name::text').get(),
+            'seller_rating': response.css('.rating-positive::text').get(),
+            'delivery_rating' : response.css('.info-content:nth-child(2) .seller-info-value::text').get(),
+            'stock': response.css('#module_quantity-input input').xpath('@max').get()
+        }
+
 
 
 
